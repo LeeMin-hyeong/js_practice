4c60999974f8f025fd1aadb3bda0e4afbb3e25ba
lmhstrar070 <lmhstrar070@naver.com>
2021-07-20 03:16:00 +0900

    210720

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -8,7 +8,15 @@ button.addEventListener("click", login);
 
 function login() {
     const req={
-        id : id.value,
-        password : password.value,
+        id: id.value,
+        password: password.value,
     };
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req)
+    });
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,12 +1,21 @@
-const home=function(req, res) {
+"use restrict";
+
+const output={
+    home: (req, res)=> {
     res.render("home/index");
+    },
+    login: (req, res)=> {
+    res.render("home/login");
+    },
 };
 
-const login=function(req, res) {
-    res.render("home/login");
+const process={
+    login: (req, res)=> {
+        console.log(req.body);
+    }
 };
 
 module.exports={
-    home,
-    login,
+    output,
+    process,
 };
